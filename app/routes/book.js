c9c7653d06bb9e036d0518ffa669af9523f85ed3
Brian Archibald <brian.archibald@imtapps.com>
2019-04-19 11:49:41 -0500

    can update book

--- app/routes/book.js
+++ app/routes/book.js
@@ -11,6 +11,10 @@ model(book) {
   setupController(controller, model) {
   this._super(controller, model);
   this.controller.set('confirmingDelete', false);
+  this.controller.set('isEditing', false);
+  this.controller.set('form.title', model.get('title'));
+  this.controller.set('form.author', model.get('author'));
+  this.controller.set('form.description', model.get('description'));
 },
 
   actions: {
@@ -19,7 +23,15 @@ model(book) {
     book.save().then(() => {
       this.transitionTo('books');
     });
+  },
+    update(book) {
+      const form = this.controller.get('form');
+      book.set('title', form.title);
+      book.set('author', form.author);
+      book.set('description', form.description);
+      book.save().then(() => {
+        this.controller.set('isEditing', false);
+    });
   }
 }
-
 });
